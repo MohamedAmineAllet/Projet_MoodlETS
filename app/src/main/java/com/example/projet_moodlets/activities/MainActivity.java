847f3f33e0d4b50b22049d34dbe35f2838907fad
Marie-Claire Lajeunesse <mclajeunesse08@gmail.com>
2026-04-04 12:42:16 -0400

    Ajout de la page MesTravauxActivity

--- app/src/main/java/com/example/projet_moodlets/activities/MainActivity.java
+++ app/src/main/java/com/example/projet_moodlets/activities/MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.projet_moodlets;
+package com.example.projet_moodlets.activities;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.projet_moodlets.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/projet_moodlets/activities/MesTravauxActivity.java
@@ -0,0 +1,16 @@
+package com.example.projet_moodlets.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projet_moodlets.R;
+
+public class MesTravauxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mes_travaux_activity);
+    }
+}
