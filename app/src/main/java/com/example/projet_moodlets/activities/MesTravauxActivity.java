75529cae48b916315b9500cd63f9953ba5ce20f7
Marie-Claire Lajeunesse <mclajeunesse08@gmail.com>
2026-04-06 22:25:55 -0400

    Ajouter la fonction de filtre et de recherche pour les travaux

--- app/src/main/java/com/example/projet_moodlets/activities/MesTravauxActivity.java
+++ app/src/main/java/com/example/projet_moodlets/activities/MesTravauxActivity.java
@@ -1,13 +1,19 @@
 package com.example.projet_moodlets.activities;
 
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.projet_moodlets.R;
@@ -20,28 +26,61 @@ import java.io.IOException;
 import java.util.List;
 
 public class MesTravauxActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-
     private ListView lv;
+    private TravauxAdapter adapteur;
+    private TextView btnTousTravaux, btnAFaire, btnEnRetard, btnRemis, btnCorrige;
+
+    private EditText editTextRecherche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mes_travaux_activity);
 
-        //TravailLocalDao daoTravail = TravailLocalDao.getInstance();
-
-       // List<Travail> listeDesTravaux = daoTravail.getTravail();
-
-       // TravauxAdapter travauxAdapter;
-
-       // travauxAdapter =  new TravauxAdapter(this, R.layout.list_travail, listeDesTravaux);
-
         lv = findViewById(R.id.lvTravaux);
-       // lv.setAdapter(travauxAdapter);
+        btnTousTravaux = findViewById(R.id.txt_filtre_travail);
+        btnAFaire = findViewById(R.id.txt_filtre_A_Faire);
+        btnEnRetard = findViewById(R.id.txt_filtre_En_Retard);
+        btnRemis = findViewById(R.id.txt_filtre_Remis);
+        btnCorrige = findViewById(R.id.txt_filtre_Corrige);
+        editTextRecherche = findViewById(R.id.edit_txt_recherche_travail);
+
+        View.OnClickListener ecouteurFiltres = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                onFiltreClick(v);
+            }
+        };
+
+        btnTousTravaux.setOnClickListener(ecouteurFiltres);
+        btnAFaire.setOnClickListener(ecouteurFiltres);
+        btnEnRetard.setOnClickListener(ecouteurFiltres);
+        btnRemis.setOnClickListener(ecouteurFiltres);
+        btnCorrige.setOnClickListener(ecouteurFiltres);
+
+
 
         lv.setOnItemClickListener(this);
 
 
+        editTextRecherche.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(adapteur != null){
+                    adapteur.rechercher(s.toString());
+                }
+            }
+        });
     }
 
     @Override
@@ -74,11 +113,33 @@ public class MesTravauxActivity extends AppCompatActivity implements AdapterView
                 MesTravauxActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        TravauxAdapter adapteur = new TravauxAdapter(MesTravauxActivity.this, R.layout.list_travail, travaux);
+                        adapteur = new TravauxAdapter(MesTravauxActivity.this, R.layout.list_travail, travaux);
                         lv.setAdapter(adapteur);
                     }
                 });
             }
         }.start();
+    }
+
+    private void onFiltreClick(View view){
+        TextView[] tousFiltres = {btnTousTravaux,
+                                    btnAFaire,
+                                    btnEnRetard,
+                                    btnRemis,
+                                    btnCorrige
+        };
+        
+        TextView tvActif = (TextView) view;
+        String filtre = tvActif.getText().toString();
+        for (TextView tv: tousFiltres) {
+            tv.setBackgroundResource(R.drawable.filtre_travail_ferme);
+            tv.setTextColor(Color.parseColor("#B3212022"));
+        }
+
+
+        view.setBackgroundResource(R.drawable.filtre_travail_active);
+
+        adapteur.filtrer(filtre);
+        Toast.makeText(this, "Filtre : " + filtre, Toast.LENGTH_SHORT).show();
     }
 }
