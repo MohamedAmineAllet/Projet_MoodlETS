08a3421e962fd11d0ee8cf825a0a69c5901ffda2
annabellecard3nas <119866562+annabellecard3nas@users.noreply.github.com>
2026-04-06 21:05:18 -0400

    modification du nom d'un dossier

--- app/src/main/java/com/example/projet_moodlets/daos/TravauxDao.java
+++ app/src/main/java/com/example/projet_moodlets/daos/TravauxDao.java
@@ -1,6 +1,6 @@
 package com.example.projet_moodlets.daos;
 
-import com.example.projet_moodlets.modeles.Travail;
+import com.example.projet_moodlets.entites.Travail;
 
 import java.util.List;
 
---
+++ app/src/main/java/com/example/projet_moodlets/entites/Cours.java
@@ -0,0 +1,12 @@
+package com.example.projet_moodlets.entites;
+
+public class Cours {
+    private  int id;
+    private String codeCours, nomCours, session, nomProfesseur;
+
+    public Cours(){
+
+    }
+
+
+}
